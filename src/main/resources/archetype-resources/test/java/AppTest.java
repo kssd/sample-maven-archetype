39f46616b097cb69164f4301b55d447276eacdd4
Kunwar Sangram Singh <kunwar.sangram@gmail.com>
2014-11-12 00:49:52 +0530

    Added Project archetype with TestNG + Logback + main/test/generated project structure

---
+++ src/main/resources/archetype-resources/test/java/AppTest.java
@@ -0,0 +1,15 @@
+package $package;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertTrue( true );
+    }
+}
+
+
